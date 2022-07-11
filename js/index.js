a9d092d7ef1fdb0f8f80a5158531c76e32c5eaae
Upal Barua <upalbarua52@gmail.com>
2022-07-11 15:48:31 +0600

    Completed navbar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-console.log('Hello world!');
+const menuToggle = document.querySelector('#navbar__toggle-btn');
+const navbar = document.querySelector('#navbar');
+
+menuToggle.addEventListener('click', () => {
+  navbar.classList.toggle('menu-visible');
+})
