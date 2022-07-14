71993620c7e54b6135a45fce8b321764239157c0
Upal Barua <upalbarua52@gmail.com>
2022-07-15 00:17:47 +0600

    Made some slight modifications

--- js/index.js
+++ js/index.js
@@ -3,4 +3,9 @@ const navbar = document.querySelector('#navbar');
 
 menuToggle.addEventListener('click', () => {
   navbar.classList.toggle('menu-visible');
+  if (navbar.classList.contains('menu-visible')) {
+    navbar.style.maxHeight = navbar.scrollHeight + 'px';
+  } else {
+    navbar.style.maxHeight = '8rem';
+  }
 });
